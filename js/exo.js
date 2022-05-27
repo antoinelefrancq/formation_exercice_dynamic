7f3124ca2c9bea89473da846f198b409d4da1eda
antoinelefrancq <antoine.lefrancq.manul@gmail.com>
2022-05-27 09:04:42 +0200

    step2

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,12 @@
 console.log('test 1...2...1...2');
+
+let hercule = {
+    name:'Hercule',
+    job:'Demi-Dieu',
+    age:35,
+    department:75,
+    arm:60.5,
+    inRelationship:true,
+}
+
+base.fillProfil(hercule)
