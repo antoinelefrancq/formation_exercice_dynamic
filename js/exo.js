9082c596a99e6238548ff4a7a24bcc909aa3b39f
antoinelefrancq <antoine.lefrancq.manul@gmail.com>
2022-05-27 09:36:35 +0200

    step8

--- js/exo.js
+++ js/exo.js
@@ -1,5 +1,6 @@
+//Step1
 console.log('test 1...2...1...2');
-
+//Step2
 const hercule = {
     name:'Hercule',
     job:'Demi-Dieu',
@@ -8,28 +9,27 @@ const hercule = {
     arm:60.5,
     inRelationship:true,
 }
-
 base.fillProfil(hercule)
-
+//Step3
 friends=[
     'Jupiter', 'Junon', 'Alcmène', 'Déjanire',
 ]
 base.printFriends(friends)
 base.setBestFriend(friends[0])
-
+//Step4
 let titleHtmlElement = document.createElement('h1')
 titleHtmlElement.classList.add('banner__title')
 titleHtmlElement.innerText=('Vous consultez le profil de Hercule')
 let bannerHtmlElement = document.getElementById('header-banner')
 bannerHtmlElement.appendChild(titleHtmlElement)
-
+//Step5
 const travaux=()=> {
     for(i=0;i<11;i++){
         base.displayWork(i)
     }
 }
 travaux()
-
+//Step6
 function dispo(heure){
     let availabilityHtmlElement =document.getElementById('availability')
     if (heure>=8 &&heure<=20){
@@ -41,16 +41,20 @@ function dispo(heure){
     }
 }
 dispo(base.getHour())
-
+//Step7
 const pseudo=(firstName,departmentNumber)=>`${firstName}-du-${departmentNumber}`
 let profilNameHtmlElement=document.getElementById('profil-name')
 profilNameHtmlElement.innerText=(pseudo(hercule.name,hercule.department))
-
+//Step8
 const toggleBanner =()=>{
     let headerHtmlElement = document.getElementById('header-banner')
     headerHtmlElement.classList.toggle('banner--open')
 }
-
 let menuHtmlElement=document.getElementById('menu-toggler')
 menuHtmlElement.addEventListener('click',toggleBanner)
-
+//Step9
+let contactHtmlElement=document.querySelector('#contact')
+contactHtmlElement.addEventListener('submit',function(event){
+    event.preventDefault()
+    alert('Hercule ne souhaite pas être dérangé')
+})
